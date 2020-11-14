ebf276cb563da1c04fe76b0fc0e7c8e0f9571124
Imen Zighed <zighedimen921@gmail.com>
2020-11-14 20:37:50 +0100

    add animation

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 class ListItem extends Component{
+myRef=React.createRef();
 
     onRemove=()=>{
-        this.props.handleDelete();
+       this.myRef.current.className="active";
+        setTimeout(()=>{
+            this.props.handleDelete();
+        },200)
+        
     }
     render(){
         const {item, handleDelete} = this.props;
         return(
             <ul>
-            <li>{item}
+            <li ref={this.myRef}>{item}
             <div className="row">
                 <i className="fa fa-pencil"
                 title="Edit" />
